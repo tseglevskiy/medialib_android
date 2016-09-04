4f065f838ba652a0e70e73224608c340d5d1583f
Igor Tseglevskiy <igor.tseglevskiy@gmail.com>
2016-09-04 21:57:06 +0300

    libraries

--- app/src/main/java/ru/roscha_akademii/medialib/MainActivity.java
+++ app/src/main/java/ru/roscha_akademii/medialib/MainActivity.java
@@ -1,13 +1,21 @@
 package ru.roscha_akademii.medialib;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import ru.roscha_akademii.medialib.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.hello.setOnClickListener(v -> Log.d("happy", "clicked"));
     }
 }
