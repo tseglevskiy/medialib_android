9a3f801c9402ff0a4d28cbb10df3d78c1189e7e7
Igor Tseglevskiy <igor.tseglevskiy@gmail.com>
2016-10-11 17:01:27 +0300

    version upgrade, :net module deleted

---
+++ app/src/androidTest/java/ru/roscha_akademii/medialib/main/view/MainActivityTestIvanP.java
@@ -0,0 +1,22 @@
+package ru.roscha_akademii.medialib.main.view;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tse on 10/10/16.
+ */
+public class MainActivityTestIvanP {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void onStart() throws Exception {
+        assertTrue(false);
+    }
+
+}
