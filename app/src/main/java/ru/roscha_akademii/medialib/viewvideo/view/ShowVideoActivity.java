caaf8ca2bb36e2e314cbbc77dad08e2b37fa1895
Igor Tseglevskiy <igor.tseglevskiy@gmail.com>
2016-10-20 01:34:56 +0300

    toggle full screen button

--- app/src/main/java/ru/roscha_akademii/medialib/viewvideo/view/ShowVideoActivity.java
+++ app/src/main/java/ru/roscha_akademii/medialib/viewvideo/view/ShowVideoActivity.java
@@ -2,6 +2,7 @@ package ru.roscha_akademii.medialib.viewvideo.view;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,25 +10,27 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.text.Cue;
 import com.google.android.exoplayer2.text.TextRenderer;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.hannesdorfmann.mosby.mvp.MvpActivity;
-
-import java.util.List;
 
 import javax.inject.Inject;
 
@@ -49,6 +52,11 @@ public class ShowVideoActivity
     DataSource.Factory dataSourceFactory;
     ExtractorsFactory extractorsFactory;
     PlayerHandler mainHandler;
+
+    private int savedOrientation;
+    private ViewGroup.LayoutParams originalContainerLayoutParams;
+    private boolean isFullscreen = false;
+    private View container;
 
     public static Intent getStartIntent(Context context, long id) {
         Intent intent = new Intent(context, ShowVideoActivity.class);
@@ -72,7 +80,7 @@ public class ShowVideoActivity
                                        float pixelWidthHeightRatio)
         {
             Log.d("happy", "video onVideoSizeChanged" + width + " " + height
-            + " " + unappliedRotationDegrees + " " + pixelWidthHeightRatio);
+                    + " " + unappliedRotationDegrees + " " + pixelWidthHeightRatio);
 
             binding.textureContainer
                     .setAspectRatio(height == 0 ? 1 : (width * pixelWidthHeightRatio) / height);
@@ -93,8 +101,10 @@ public class ShowVideoActivity
 
         super.onCreate(savedInstanceState);
 
+        Log.d("happy", "onCreate");
+
         binding = DataBindingUtil.setContentView(this, R.layout.showvideo_activity);
-        binding.videoId.setText("" + getVideoId());
+//        binding.videoId.setText("" + getVideoId());
 
         mainHandler = new PlayerHandler();
 
@@ -117,6 +127,12 @@ public class ShowVideoActivity
         );
 
         extractorsFactory = new DefaultExtractorsFactory();
+
+//        container = binding.container;
+        container = binding.textureContainer;
+
+        originalContainerLayoutParams = container.getLayoutParams();
+        binding.videoId.setOnClickListener(v -> doToggleFullscreen());
 
     }
 
@@ -171,5 +187,86 @@ public class ShowVideoActivity
         }
     }
 
+    public void doToggleFullscreen() {
+        if (isFullscreen) {
+            Log.d("happy", "go normal");
+            setRequestedOrientation(savedOrientation);
+
+            getSupportActionBar().show();
+
+            // Make the status bar and navigation bar visible again.
+            getWindow().getDecorView().setSystemUiVisibility(0);
+
+            container.setLayoutParams(originalContainerLayoutParams);
+
+            getWindow().setFlags(
+                    0, // clear flag
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+            container.setLayoutParams(getLayoutParamsBasedOnParent(
+                    container,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
+//            fullscreenButton.setImageResource(R.drawable.ic_action_full_screen);
+
+            isFullscreen = false;
+        } else {
+            Log.d("happy", "go full");
+            savedOrientation = getResources().getConfiguration().orientation;
+
+            getSupportActionBar().hide();
+
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+//            requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+            container.setLayoutParams(getLayoutParamsBasedOnParent(
+                    container,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+
+//            fullscreenButton.setImageResource(R.drawable.ic_action_return_from_full_screen);
+
+            isFullscreen = true;
+        }
+
+    }
+
+    public static ViewGroup.LayoutParams getLayoutParamsBasedOnParent(View view, int width, int height)
+            throws IllegalArgumentException
+    {
+
+        // Get the parent of the given view.
+        ViewParent parent = view.getParent();
+
+        // Determine what is the parent's type and return the appropriate type of LayoutParams.
+        if (parent instanceof FrameLayout) {
+            return new FrameLayout.LayoutParams(width, height);
+        }
+        if (parent instanceof RelativeLayout) {
+            return new RelativeLayout.LayoutParams(width, height);
+        }
+        if (parent instanceof LinearLayout) {
+            return new LinearLayout.LayoutParams(width, height);
+        }
+
+        // Throw this exception if the parent is not the correct type.
+        IllegalArgumentException exception = new IllegalArgumentException("The PARENT of a " +
+                "FrameLayout container used by the GoogleMediaFramework must be a LinearLayout, " +
+                "FrameLayout, or RelativeLayout. Please ensure that the container is inside one of these " +
+                "three supported view groups.");
+
+        // If the parent is not one of the supported types, throw our exception.
+        throw exception;
+    }
 
 }
